61216d6d86657b3e525ff799a275d46fecedfba4
Glitch (lean-chief-cough) <none>
2021-11-03 13:45:35 +0000

    🏤⛲️ Checkpoint ./google-doc-url.js:38188679/196 ./style.css:38188679/63

---
+++ google-doc-url.js
@@ -0,0 +1,5 @@
+// paste in your published Google Sheets URL from the browser address bar
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1a2d1CtuZzByzvA0kHcfJe9m8WzN6SFADz6SED6DHxBk/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyD-7q3vvmecpTSG0QlZYZXG8ezplDVmxgQ';
